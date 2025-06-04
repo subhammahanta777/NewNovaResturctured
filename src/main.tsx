1515d3bf86f903726273479445a9902b80dde104
Subham Mahanta <subham.mahanta777@gmail.com>
2025-06-04 12:47:14 +0530

    Create main.tsx entry point

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NovaAdminConsole from './components/NovaAdminConsole'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <NovaAdminConsole />
+  </React.StrictMode>,
+)
